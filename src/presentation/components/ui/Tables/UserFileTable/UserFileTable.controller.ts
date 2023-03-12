ea6a12c3fd759457a3353d1a999dbcb024386031
Silviu <silviupantelimon.say10@gmail.com>
2023-03-12 13:59:53 +0200

    Adds more comments

--- src/presentation/components/ui/Tables/UserFileTable/UserFileTable.controller.ts
+++ src/presentation/components/ui/Tables/UserFileTable/UserFileTable.controller.ts
@@ -16,21 +16,26 @@ const getFileContent = (filename?: string) => {
     }
 }
 
+/**
+ * This is controller hook manages the table state including the pagination and data retrieval from the backend.
+ */
 export const useUserFileTableController = () => {
-    const { getUserFiles: { key: queryKey, query }, downloadUserFile: { query: download } } = useUserFileApi();
-    const queryClient = useQueryClient();
-    const { page, pageSize, setPagination } = usePaginationController();
-    const { data, isError, isLoading } = useQuery([queryKey, page, pageSize], () => query({ page, pageSize }));
-    const downloadUserFile = useCallback((userFile: UserFileDTO) => download(userFile.id ?? '').then((data) => downloadDocument(data, userFile.name ?? '')), [download]);
-    const openUserFile = useCallback((userFile: UserFileDTO) => download(userFile.id ?? '').then((data) => userFile.name?.endsWith(".pdf") ? openDocument(data, getFileContent(userFile.name)) : openDocument(data)), [download]);
+    const { getUserFiles: { key: queryKey, query }, downloadUserFile: { query: download } } = useUserFileApi(); // Use the API hook.
+    const queryClient = useQueryClient(); // Get the query client.
+    const { page, pageSize, setPagination } = usePaginationController(); // Get the pagination state.
+    const { data, isError, isLoading } = useQuery([queryKey, page, pageSize], () => query({ page, pageSize })); // Retrieve the table page from the backend via the query hook.
+    const downloadUserFile = useCallback((userFile: UserFileDTO) => download(userFile.id ?? '')
+        .then((data) => downloadDocument(data, userFile.name ?? '')), [download]); // Create the callback to download the user file.
+    const openUserFile = useCallback((userFile: UserFileDTO) => download(userFile.id ?? '')
+        .then((data) => userFile.name?.endsWith(".pdf") ? openDocument(data, getFileContent(userFile.name)) : openDocument(data)), [download]); // Create the callback to open the user file in another tab.
 
     const tryReload = useCallback(
         () => queryClient.invalidateQueries([queryKey]),
-        [queryClient, queryKey]);
+        [queryClient, queryKey]); // Create a callback to try reloading the data for the table via query invalidation.
 
-    const tableController = useTableController(setPagination, data?.response?.pageSize);
+    const tableController = useTableController(setPagination, data?.response?.pageSize); // Adapt the pagination for the table.
 
-    return {
+    return { // Return the controller state and actions.
         ...tableController,
         tryReload,
         pagedData: data?.response,
--- src/presentation/components/ui/Tables/UserTable/UserTable.controller.ts
+++ src/presentation/components/ui/Tables/UserTable/UserTable.controller.ts
@@ -4,23 +4,26 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useCallback } from "react";
 import { usePaginationController } from "../Pagination.controller";
 
+/**
+ * This is controller hook manages the table state including the pagination and data retrieval from the backend.
+ */
 export const useUserTableController = () => {
-    const { getUsers: { key: queryKey, query }, deleteUser: { key: deleteUserKey, mutation: deleteUser } } = useUserApi();
-    const queryClient = useQueryClient();
-    const { page, pageSize, setPagination } = usePaginationController();
-    const { data, isError, isLoading } = useQuery([queryKey, page, pageSize], () => query({ page, pageSize }));
-    const { mutateAsync: deleteMutation } = useMutation([deleteUserKey], deleteUser);
+    const { getUsers: { key: queryKey, query }, deleteUser: { key: deleteUserKey, mutation: deleteUser } } = useUserApi(); // Use the API hook.
+    const queryClient = useQueryClient(); // Get the query client.
+    const { page, pageSize, setPagination } = usePaginationController(); // Get the pagination state.
+    const { data, isError, isLoading } = useQuery([queryKey, page, pageSize], () => query({ page, pageSize })); // Retrieve the table page from the backend via the query hook.
+    const { mutateAsync: deleteMutation } = useMutation([deleteUserKey], deleteUser); // Use a mutation to remove an entry.
     const remove = useCallback(
         (id: string) => deleteMutation(id).then(() => queryClient.invalidateQueries([queryKey])),
-        [queryClient, deleteMutation, queryKey]);
+        [queryClient, deleteMutation, queryKey]); // Create the callback to remove an entry.
 
     const tryReload = useCallback(
         () => queryClient.invalidateQueries([queryKey]),
-        [queryClient, queryKey]);
+        [queryClient, queryKey]); // Create a callback to try reloading the data for the table via query invalidation.
 
-    const tableController = useTableController(setPagination, data?.response?.pageSize);
+    const tableController = useTableController(setPagination, data?.response?.pageSize); // Adapt the pagination for the table.
 
-    return {
+    return { // Return the controller state and actions.
         ...tableController,
         tryReload,
         pagedData: data?.response,
